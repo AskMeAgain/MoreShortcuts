a2fc9d190ee54f26c775c7b54fe49433d1160044
AskMeAgain <lol@lol.com>
2021-10-12 21:55:49 +0200

    Fixing docu

--- src/test/java/ask/me/again/shortcut/additions/introducemock/introducemockvariable/IntroduceMockVariableExample.java
+++ src/test/java/ask/me/again/shortcut/additions/introducemock/introducemockvariable/IntroduceMockVariableExample.java
@@ -1,19 +1,11 @@
 package ask.me.again.shortcut.additions.introducemock.introducemockvariable;
 
 import ask.me.again.shortcut.additions.introducemock.entities.TestClass;
-import org.mockito.Mock;
-import org.mockito.Mock;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.Mockito;
-import org.mockito.Mockito;
 
 class IntroduceMockVariableExample {
 
   void example() {
-    var string = Mockito.mock(String.class);
-    var integer = Mockito.mock(Integer.class);
-    var result = TestClass.staticMethod(integer, string);
+    var result = TestClass.staticMethod(null, null);
   }
 
 }
