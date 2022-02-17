8b75bd65d2d2624658d088345994fe5a927d09d5
AskMeAgain <20632124+AskMeAgain@users.noreply.github.com>
2022-02-17 19:09:04 +0100

    second (working) version

--- src/main/java/ask/me/again/shortcut/additions/naming/NamingSchemeSettingsPanel.java
+++ src/main/java/ask/me/again/shortcut/additions/naming/NamingSchemeSettingsPanel.java
@@ -3,7 +3,6 @@ package ask.me.again.shortcut.additions.naming;
 import ask.me.again.shortcut.additions.multilinemagic.MultilineUtils;
 import ask.me.again.shortcut.additions.settings.SettingsCreator;
 import com.intellij.ide.util.PropertiesComponent;
-import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import java.awt.event.ItemEvent;
@@ -13,31 +12,27 @@ import java.util.Map;
 public class NamingSchemeSettingsPanel implements SettingsCreator {
 
   private final PropertiesComponent instance;
+  private final Map<String, Boolean> allowedNamingSchemes = new HashMap<>();
 
   public NamingSchemeSettingsPanel(PropertiesComponent propertiesComponent) {
     this.instance = propertiesComponent;
 
-    System.out.println(instance.getValue("a"));
-
     allowedNamingSchemes.put("SNAKE_CASE", getBooleanFromPersistance(computeName("SNAKE_CASE")));
-    allowedNamingSchemes.put("doener-case", getBooleanFromPersistance(computeName("doener-case")));
-    allowedNamingSchemes.put("dot.case", getBooleanFromPersistance(computeName("dot.case")));
-    allowedNamingSchemes.put("PascalCase", getBooleanFromPersistance(computeName("PascalCase")));
-    allowedNamingSchemes.put("camelCase", getBooleanFromPersistance(computeName("camelCase")));
+//    allowedNamingSchemes.put("doener-case", getBooleanFromPersistance(computeName("doener-case")));
+//    allowedNamingSchemes.put("dot.case", getBooleanFromPersistance(computeName("dot.case")));
+//    allowedNamingSchemes.put("PascalCase", getBooleanFromPersistance(computeName("PascalCase")));
+//    allowedNamingSchemes.put("camelCase", getBooleanFromPersistance(computeName("camelCase")));
   }
 
-  @NotNull
   private String computeName(String name) {
-    return "shortcut_addition_naming_scheme_" + name;
+    return "shortcut_naming_scheme_" + name;
   }
 
   private boolean getBooleanFromPersistance(String path) {
-    var result = instance.getBoolean(path, true);
+    var result = instance.getBoolean(path, false);
     System.out.println("Persistance: " + path + " -> " + result);
     return result;
   }
-
-  private final Map<String, Boolean> allowedNamingSchemes = new HashMap<>();
 
   public String getName() {
     return "Naming Schemes";
@@ -53,11 +48,11 @@ public class NamingSchemeSettingsPanel implements SettingsCreator {
       checkBox.setSelected(isEnabled);
       checkBox.addItemListener(e -> {
         var v1 = e.getStateChange() == ItemEvent.SELECTED;
-        System.out.println("Voted: " + k + " -> " + v1);
+        System.out.println("Voted: '" + computeName(k) + "' -> " + v1);
         allowedNamingSchemes.put(k, v1);
       });
 
-      System.out.println("On Start: " + k + " -> " + isEnabled);
+      System.out.println("On Start: '" + k + "' -> " + isEnabled);
       jPanel.add(checkBox);
     });
 
@@ -71,12 +66,8 @@ public class NamingSchemeSettingsPanel implements SettingsCreator {
 
   @Override
   public void save() {
-    var instance = PropertiesComponent.getInstance();
-
-    instance.setValue("a", "testvalue");
-
     allowedNamingSchemes.forEach((k, v) -> {
-      System.out.println("Saved: " + k + " -> " + v);
+      System.out.println("Saved: '" + computeName(k) + "' -> " + v);
       instance.setValue(computeName(k), v);
     });
   }
