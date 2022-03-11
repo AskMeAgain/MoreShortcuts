e857a11d95fa4040098d7409620e62ba08cf5d37
AskMeAgain <20632124+AskMeAgain@users.noreply.github.com>
2022-03-11 21:46:35 +0100

    Settings window v1 done

--- src/main/java/ask/me/again/shortcut/additions/mockswitchtype/SwitchMockingVariantUtils.java
+++ src/main/java/ask/me/again/shortcut/additions/mockswitchtype/SwitchMockingVariantUtils.java
@@ -9,23 +9,22 @@ public class SwitchMockingVariantUtils {
 
   public static String convertLine(String text, Boolean addStaticImport) {
     var matcherCase1 = case1Pattern.matcher(text);
+    var matcherCase2 = case2Pattern.matcher(text);
+
     if (matcherCase1.find() && matcherCase1.groupCount() == 3) {
       var mockName = matcherCase1.group(1);
       var methodName = matcherCase1.group(2);
       var object = matcherCase1.group(3);
       text = String.format("Mockito.doReturn(%s).when(%s).%s;", object, mockName, methodName);
-    }
-
-    var matcherCase2 = case2Pattern.matcher(text);
-    if (matcherCase2.find() && matcherCase2.groupCount() == 3) {
+    } else if (matcherCase2.find() && matcherCase2.groupCount() == 3) {
       var object = matcherCase2.group(1);
       var mockName = matcherCase2.group(2);
       var methodName = matcherCase2.group(3);
       text = String.format("Mockito.when(%s.%s).thenReturn(%s);", mockName, methodName, object);
     }
 
-    if(addStaticImport){
-      text = text.replaceAll("Mockito\\.","");
+    if (addStaticImport) {
+      text = text.replaceAll("Mockito\\.", "");
     }
 
     return text;
