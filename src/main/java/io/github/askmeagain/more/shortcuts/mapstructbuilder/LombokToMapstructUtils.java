7d6b2cbf596688ec760dd21a3abde29d2113ffa7
AskMeAgain <20632124+AskMeAgain@users.noreply.github.com>
2022-07-24 16:14:30 +0200

    More refactorings v8

--- src/main/java/io/github/askmeagain/more/shortcuts/mapstructbuilder/LombokToMapstructUtils.java
+++ src/main/java/io/github/askmeagain/more/shortcuts/mapstructbuilder/LombokToMapstructUtils.java
@@ -1,19 +1,13 @@
 package io.github.askmeagain.more.shortcuts.mapstructbuilder;
 
 import com.intellij.openapi.project.Project;
-import com.intellij.psi.JavaRecursiveElementVisitor;
 import com.intellij.psi.PsiExpressionList;
-import com.intellij.psi.PsiPolyadicExpression;
 import com.intellij.psi.PsiType;
-import com.intellij.psi.impl.source.tree.java.PsiReferenceExpressionImpl;
 import com.intellij.psi.search.GlobalSearchScope;
 import io.github.askmeagain.more.shortcuts.mapstructbuilder.entities.InputObjectContainer;
-import io.github.askmeagain.more.shortcuts.mapstructbuilder.printer.MapStructAnnotation;
-import io.github.askmeagain.more.shortcuts.mapstructbuilder.printer.MapStructMethod;
-import org.apache.commons.lang3.StringUtils;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LombokToMapstructUtils {
 
